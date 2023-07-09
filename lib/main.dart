fc4f1d66685fda602e0586aa50828540b3162857
unknown <rachmatashari13@gmail.com>
2023-07-09 11:30:56 +0700

    Menambahkan tugas pertemuan 9 versi 3

---
+++ lib/main.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_pertemuan9v3/routes.dart';
+void main() {
+runApp(const MaterialApp(
+onGenerateRoute: RouteGenerator.generateRoute,
+));
+}
